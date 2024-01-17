4e6ed00a13c96ebcae93795e9ec2d8922e11814e
BuildTools <kkop3228@flhs.us>
2024-01-16 20:29:06 -0500

    Added Base Classes for setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Auto/pathFollower.java
@@ -0,0 +1,2 @@
+package org.firstinspires.ftc.teamcode.opModes.Auto;public class pathFollower {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pathGeneration/pathFinder.java
@@ -0,0 +1,2 @@
+package org.firstinspires.ftc.teamcode.pathGeneration;public class pathFinder {
+}
